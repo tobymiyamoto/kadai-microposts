b1c3f3da394dc1a1df5477f101579e6d1f4ba3ec
tsubasa.miyamoto <tobymiyamoto@gmail.com>
2019-03-02 19:53:19 +0000

    correct

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <aside class="col-sm-4">
             @include('users.card', ['user' => $user])
-        </aside>
+         </aside>
         <div class="col-sm-8">
             @include('users.navtabs', ['user' => $user])
             @if (Auth::id() == $user->id)
